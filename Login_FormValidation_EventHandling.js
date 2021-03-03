4b9f1d3960ecf75d1293bc52d35a46c70d695ba1
Daniel Kovacs <kovacsdansan@gmail.com>
2021-03-03 09:25:41 +0200

    jQuery_merging_01

--- Login_FormValidation_EventHandling.js
+++ Login_FormValidation_EventHandling.js
@@ -1,33 +1,32 @@
 const form = document.getElementById('form');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
+const form_username = document.getElementById('form_username');
+const form_password = document.getElementById('form_password');
 
 form.addEventListener('submit', e => {
 	e.preventDefault();
-	
+
 	checkInputs();
 });
 
 function checkInputs() {
 	// trim to remove the whitespaces
-	const emailValue = email.value.trim();
-	const passwordValue = password.value.trim();
-	
-	if(emailValue === '') {
-		setErrorFor(email, 'Email cannot be blank');
-	} else if (!isEmail(emailValue)) {
-		setErrorFor(email, 'Not a valid email');
-	} else {
-		setSuccessFor(email);
+	const usernameValue = form_username.value.trim();
+	const passwordValue = form_password.value.trim();
+
+	if (usernameValue === '') {
+		setErrorFor(form_username, 'Username cannot be blank');
 	}
-	
-	if(passwordValue === '') {
-		setErrorFor(password, 'Password cannot be blank');
+	else {
+		setSuccessFor(form_username);
 	}
-    else {
-		setSuccessFor(password);
+
+	if (passwordValue === '') {
+		setErrorFor(form_password, 'Password cannot be blank');
 	}
-	
+	else {
+		setSuccessFor(form_password);
+	}
+
 }
 
 function setErrorFor(input, message) {
@@ -40,8 +39,4 @@ function setErrorFor(input, message) {
 function setSuccessFor(input) {
 	const formControl = input.parentElement;
 	formControl.className = 'form-control success';
-}
-	
-function isEmail(email) {
-	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
